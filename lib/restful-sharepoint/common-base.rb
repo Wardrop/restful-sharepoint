e434d0e6249702c52a22def44740a3258826760f
Tom Wardrop <tom@tomwardrop.com>
2018-01-24 08:39:32 +1000

    v0.1.5. Missing metadata type now raises an exception.

--- lib/restful-sharepoint/common-base.rb
+++ lib/restful-sharepoint/common-base.rb
@@ -2,16 +2,15 @@ module RestfulSharePoint
   class CommonBase
     # Converts the given enumerable tree to a collection or object.
     def objectify(tree)
-      klass = nil
       if tree['results'] && !tree['results'].empty?
-        if type = tree.dig('__metadata', 'type')  || tree.dig('results', 0, '__metadata', 'type')
-          pattern, klass = COLLECTION_MAP.find { |pattern,| pattern.match(type)  }
-        end
+        type = tree.dig('__metadata', 'type')  || tree.dig('results', 0, '__metadata', 'type')
+        raise "Object type not specified" unless type
+        pattern, klass = COLLECTION_MAP.find { |pattern,| pattern.match(type)  }
         klass ? RestfulSharePoint.const_get(klass).new(parent: self, collection: tree['results']) : tree['results']
       elsif tree['__metadata']
-        if type = tree['__metadata']['type']
-          pattern, klass = OBJECT_MAP.find { |pattern,| pattern.match(type)  }
-        end
+        type = tree['__metadata']['type']
+        raise "Collection type not specified" unless type
+        pattern, klass = OBJECT_MAP.find { |pattern,| pattern.match(type)  }
         klass ? RestfulSharePoint.const_get(klass).new(parent: self, properties: tree) : tree
       end
     end
